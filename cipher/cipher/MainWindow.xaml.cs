07b122e247a81395477a5755ee1af013a088f2fd
Piotr Kontowicz <przemyslowiec@gmail.com>
2018-10-02 22:34:03 +0200

    First commit, user interface

---
+++ cipher/cipher/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cipher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void load_file(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog op = new OpenFileDialog();
+                if (op.ShowDialog() == true)
+                {
+                    text.Text = File.ReadAllText(op.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error, somthing gone wrong.\n Orginal message:" + ex.Message);
+            }
+        }
+
+        private void save_file(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog()
+            {
+                Filter = "Text Files(*.txt)|*.txt|All(*.*)|*"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, text.Text);
+            }
+        }
+    }
+}
